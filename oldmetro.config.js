673f87a154d0822b05b54cd0117ffd3191b5e490
patgpt <doitformurphy@gmail.com>
2025-01-25 20:28:58 -0500

    retry

--- oldmetro.config.js
+++
@@ -1,6 +0,0 @@
-//metro.config.js;
-const { getDefaultConfig } = require("expo/metro-config");
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts.push("sql");
-module.exports = config;
